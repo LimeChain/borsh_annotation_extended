4fcc50dced4717257fc7cd4cf2ae5489ebfc1f48
vlady-kotsev <vladislav.kotsev@limechain.tech>
2025-10-20 15:17:10 +0300

    fix: add BBool, BPublicKey

--- lib/borsh_annotation_extended.dart
+++ lib/borsh_annotation_extended.dart
@@ -3,3 +3,4 @@ library;
 export 'src/annotations.dart';
 
 export "package:borsh_annotation/borsh_annotation.dart" hide BFixedArray;
+export "package:solana/solana.dart" show BBool, BPublicKey, Ed25519HDPublicKey;
